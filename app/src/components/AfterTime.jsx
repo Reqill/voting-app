bcfcee2554c3f9526b64825a059e81960cf2b8a1
Mikołaj Mrózek <m.mik311@gmail.com>
2021-09-25 11:29:11 +0200

    Create AfterTime.jsx

---
+++ app/src/components/AfterTime.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+const AfterTime = ({ colors }) => {
+    return (
+        <p>
+            AfterTime
+        </p>
+    )
+}
+
+export default AfterTime;
